f85e09da74b92d8e3ce562391c4f1f517be57d85
Yair Quintana <yairquintanamx@gmail.com>
2024-01-16 12:13:41 -0600

    feat(db): Add get_db generator to inject db dependency

--- db/session.py
+++ db/session.py
@@ -1,5 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from typing import Generator
 
 from core.config import settings
 
@@ -7,3 +8,11 @@ DATABASE_URL = settings.DB_URL
 engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+def get_db() -> Generator:
+    try:
+        db = SessionLocal()
+        yield db
+    finally:
+        db.close()
