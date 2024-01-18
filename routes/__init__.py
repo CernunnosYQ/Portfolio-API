6937a5ae39176267d48682fd9ef8f1b6dd9a4baa
Yair Quintana <yairquintanamx@gmail.com>
2024-01-18 00:40:17 -0600

    feat(test): Create auth tests

--- routes/__init__.py
+++ routes/__init__.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter
 
-from routes.v1 import user, blogpost
+from routes.v1 import user, blogpost, auth
 
 v1_router = APIRouter(prefix="/v1")
 v1_router.include_router(user.router, prefix="", tags=["users"])
 v1_router.include_router(blogpost.router, prefix="", tags=["blogpost"])
+v1_router.include_router(auth.router, prefix="", tags=["auth"])
---
+++ tests/test_routes/test_auth.py
@@ -0,0 +1,43 @@
+from tests.utils.user import create_test_user
+from core.security import validate_access_token
+
+data = {
+    "username": "testuser",
+    "email": "testuser@test.com",
+    "password": "TestUser123456",
+}
+
+
+def test_create_token(db_session):
+    user, token = create_test_user(db_session)
+    assert len(token) > 0, "Token is not created"
+
+    validation = validate_access_token(token)
+    assert validation.get("success"), "Token validation fails"
+    assert validation.get("payload").get("sub") == user.username
+
+
+def test_email_login(client, db_session):
+    user, _ = create_test_user(db_session, data=data)
+    response = client.post(
+        "/v1/token", data={"username": user.email, "password": data["password"]}
+    )
+    assert response.status_code == 200
+
+    response_data = response.json()
+    assert "access_token" in response_data
+    assert validate_access_token(response_data.get("access_token")).get("success")
+    assert response_data.get("token_type") == "bearer"
+
+
+def test_username_login(client, db_session):
+    user, _ = create_test_user(db_session, data=data)
+    response = client.post(
+        "/v1/token", data={"username": user.username, "password": data["password"]}
+    )
+    assert response.status_code == 200
+
+    response_data = response.json()
+    assert "access_token" in response_data
+    assert validate_access_token(response_data.get("access_token")).get("success")
+    assert response_data.get("token_type") == "bearer"
